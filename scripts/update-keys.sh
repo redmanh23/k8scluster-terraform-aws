94d932cc2e89f950d36bf538dd41cabcbdc3a3e9
Ubuntu <ubuntu@ip-172-31-85-216.ec2.internal>
2020-02-01 23:25:18 +0000

    updated and renamed some files

--- scripts/update-keys.sh
+++ scripts/update-keys.sh
@@ -8,4 +8,4 @@ while IFS= read -r line;
 do
     echo "$line" | update-ssh-keys -a $x;
     x=$(( $x + 1 ))
-done < /home/ubuntu/k8scluster-terraform-aws/scripts/authorized_keys
+done < /opt/authorized_keys
